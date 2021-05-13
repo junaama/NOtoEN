ad0cd3735a318051ecc2659eae728bdc642aef7f
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2021-05-13 13:53:42 -0400

    Add id to json

--- makejson.py
+++ makejson.py
@@ -2,14 +2,16 @@ import json
 import re
 filename = 'nb-NOtoENdictionary.txt'
 
-dict1 = {}
-
+overalldict = {}
+count = 0
 with open(filename) as fh:
     for line in fh:
+        tempdict = {}
         word, definition = line.strip().split("	", 1)
         newdef = definition.strip().split("	")
-        dict1[word] = newdef
+        tempdict[word] = newdef
+        overalldict[count] = tempdict
+        count+= 1
 out_file = open("final.json", "w")
-json.dump(dict1, out_file, indent  = 4, sort_keys = False, ensure_ascii=False)
+json.dump(overalldict, out_file, indent  = 4, sort_keys = False, ensure_ascii=False)
 out_file.close()
-
