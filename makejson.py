a0de1759f01c210061cefb0c7554aac6d8ed490f
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2021-05-17 14:27:07 -0400

    Revert json api

--- makejson.py
+++ makejson.py
@@ -4,17 +4,15 @@ filename = 'nb-NOtoENdictionary.txt'
 
 overalldict = {}
 count = 0
-overalldict["dictionary"] = []
+
 with open(filename) as fh:
     for line in fh:
         tempdict = {}
-        temparr = []
+        
         word, definition = line.strip().split("	", 1)
         newdef = definition.strip().split("	")
-        tempdict["id"] = count
-        tempdict["word"] = word
-        tempdict["definition"] = newdef
-        overalldict["dictionary"].append(tempdict)
+        tempdict[word] = newdef
+        overalldict[count] = tempdict
         count+= 1
 out_file = open("final.json", "w")
 json.dump(overalldict, out_file, indent  = 4, sort_keys = False, ensure_ascii=False)
